9f5039f4f50919347d37a7d42518d867934e3e3a
Raja Simon <coderxsimon@gmail.com>
2025-07-14 23:52:16 +0530

    Add notification

--- lognowbot/__init__.py
+++ lognowbot/__init__.py
@@ -4,24 +4,10 @@ LogNowBot Python Client Library
 A Python library for sending notifications via LogNowBot.
 """
 
-from .client import (
-    LogNowBotClient,
-    send_notification,
-    send_info,
-    send_warning,
-    send_error,
-    send_success,
-)
+from .client import LogNowBotClient, send_text
 
 __version__ = "0.1.0"
 __author__ = "LogNowBot"
-__email__ = "contact@lognowbot.com"
+__email__ = "supprot@lognowbot.com"
 
-__all__ = [
-    "LogNowBotClient",
-    "send_notification",
-    "send_info",
-    "send_warning",
-    "send_error",
-    "send_success",
-]
+__all__ = ["LogNowBotClient", "send_text"]
